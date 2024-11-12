e5de7850a540803d3c8aec234692ef3c025de31a
Alpit1207 <133636118+Alpit1207@users.noreply.github.com>
2024-11-13 01:05:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.py
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.py
@@ -1,13 +1,7 @@
 class Solution:
     def reverseArray(self, arr):
-        i = 0
-        j = len(arr)-1
-        while(i<=j):
-            arr[i],arr[j] = arr[j],arr[i]
-            i += 1
-            j -=1
-        return arr    
-        
+        arr[::] = arr[::][::-1]
+        return arr
         # code here
         
         
